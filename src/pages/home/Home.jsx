310a6f913a3649ce1ea2c7663381f57cac53fd9e
Gulam Gaush <mohammadgulamgaush@gmail.com>
2022-08-31 19:06:37 +0530

    half way through featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import '../../style/home.css'
 import Navbar from '../../components/Navbar'
 import Header from '../../components/Header'
+import Featured from '../../components/Featured'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Featured/>
+        <div className="homeTitle">Browse by property type</div>
+      </div>
     </div>
   )
 }
