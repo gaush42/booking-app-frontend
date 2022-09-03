11edde6211658b513d1a7fb2abfea5fb3c74c717
Gulam Gaush <mohammadgulamgaush@gmail.com>
2022-09-03 19:30:04 +0530

    almost done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import '../../style/home.css'
-import Navbar from '../../components/Navbar'
-import Header from '../../components/Header'
-import Featured from '../../components/Featured'
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
+import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
+import "./home.css";
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Header/>
       <div className="homeContainer">
         <Featured/>
-        <div className="homeTitle">Browse by property type</div>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+        <MailList/>
+        <Footer/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
